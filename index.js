76305d0f4b2b8b09f4879f6610ffb45690b3cec7
Adam Purcell <apurcell88@gmail.com>
2022-09-16 15:06:29 -0500

    Added more methods.

--- index.js
+++ index.js
@@ -58,12 +58,73 @@ class LinkedList {
   }
 
   pop() {
-    // removes the last element from the list
+    let current = this.head;
+    let newTail = current;
+    // if there are no nodes in the list, return undefined
+    if (this.length === 0) {
+      return undefined;
+    }
+    // loop through the list until you reach the tail
+    while (current.next) {
+      // set the tail to be the 2nd to last node
+      newTail = current;
+      current = current.next;
+    }
+    // set the next property of the 2nd to last node to be null
+    this.tail = newTail;
+    this.tail.next = null;
+    // decrement the length of the list by 1
+    this.length--;
+    // return the value of the node removed
+    return current;
+  }
+
+  contains(val) {
+    // returns true if the passed in value is in the list and otherwise false
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    while (current.next) {
+      if (current.val === val) {
+        return true;
+      }
+      current = current.next;
+    }
+
+    return false;
+  }
+
+  find(val) {
+    // returns the index of the node containing value, or null if not found
+    let index = 0;
+    let current = this.head;
+
+    while (index < this.length) {
+      if (current.val === val) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return null;
+  }
+
+  toString() {
+    // prints the LinkedList as strings so you can preview them in the console.
+    let current = this.head;
+
+    while (current.next) {
+      console.log(current.val);
+      current = current.next;
+    }
   }
 }
 
 const list = new LinkedList();
 list.push('<3');
 list.push('-_-');
-list.push('whoaaaa');
-// console.log(list);
+list.push('whoa');
+list.push('yay');
+// console.log(list.find('...'));
